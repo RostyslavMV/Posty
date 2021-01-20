0c598b2912f4820c63f5d17aa2bd9dfdfdc8acff
Rostyslav M <mrostyk55@gmail.com>
2021-01-20 17:54:42 +0200

    fixed filter (it is now get, not post request on same url)

--- src/main/java/com/rmv/posty/controller/MainController.java
+++ src/main/java/com/rmv/posty/controller/MainController.java
@@ -28,9 +28,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Model model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findMessageByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -46,19 +52,4 @@ public class MainController {
 
         return "main";
     }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Model model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findMessageByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.addAttribute("messages", messages);
-
-        return "main";
-    }
-
 }
